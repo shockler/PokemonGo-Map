4e149e491ef61c9e0b9531561be20174280c04b3
Jangles <njangula@myriadmobile.com>
2016-11-18 12:56:25 -0600

    fixed migration

--- pogom/models.py
+++ pogom/models.py
@@ -1274,5 +1274,5 @@ def database_migrate(db, old_ver):
 
     if old_ver < 10:
         migrate(
-            migrator.add_column('scannedlocation', 'username', TextField(null=True, default="")),
+            migrator.add_column('scannedlocation', 'username', CharField(max_length=255, null=True)),
         )
