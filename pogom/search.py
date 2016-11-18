c9b8b533098ece18baaea275c36d342749e9127e
Jangles <njangula@myriadmobile.com>
2016-11-17 20:51:51 -0600

    Added a login delay as suggested by @simonbuehler

--- pogom/search.py
+++ pogom/search.py
@@ -832,7 +832,7 @@ def calc_distance(pos1, pos2):
 def stagger_thread(args, account):
     if args.accounts.index(account) == 0:
         return  # No need to delay the first one
-    delay = args.accounts.index(account) + ((random.random() - .5) / 2)
+    delay = args.accounts.index(account) * 6 + ((random.random() - .5) / 2)
     log.debug('Delaying thread startup for %.2f seconds', delay)
     time.sleep(delay)
 
